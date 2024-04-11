15044b2d1370e86d4525124c152c5666a26735d3
Runaway9 <936194582@qq.com>
2024-04-11 11:02:00 +0800

    变更

--- testcase/test_login.py
+++ testcase/test_login.py
@@ -2,12 +2,16 @@ import pytest
 from selenium import webdriver
 from pageobject.login_page import Login_Page
 import time
+import allure
+import subprocess
 
-
+@allure.feature("Login")
 class Test_Login():
 
+    @allure.title("test_login")
     def test_login(self):
         self.driver = webdriver.Chrome()
         login = Login_Page(self.driver).loginpage(username='15501411920', password='Runaway9')
         time.sleep(10)
-
+        # 运行pytest，并生成allure报告
+        subprocess.run(["pytest", "--alluredir=allure-results"])
